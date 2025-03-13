93e6866e6c05d6e264fb372c840f90f40078aca6
Alayna28 <alaynabaloch100@gmail.com>
2025-03-13 20:42:07 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#take input for the studenet that he can attend the exam or not
+medical_cause=input("did you have a medical cause Y OR N: ")
+#take input of the attendance
+atten = int(input("enter the attendance of the students:"))
+#checking the user input predicting output accordingly
+
+if medical_cause == 'Y': #checking the condition 1
+    print("you are allowed")
+else:
+    if atten>=75: #checking the condition 2
+        print("allowed")
+    else:
+      print("not allowed")
